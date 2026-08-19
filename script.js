c86248cd41ab9697cf5cfd9a8693663e173448cc
henrivj <henrique_justus@estudante.sesisenai.org.br>
2026-08-19 09:51:27 -0300

    v3

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-const txt = document.getElementById("txt")
+const txt1 = document.getElementById("txt1")
+const txt2 = document.getElementById("txt2")
+const txt3 = document.getElementById("txt3")
+
 let count = 0
 
 function main() {
 	count++
-	if (count >= 45) {
-		count = 0
-		if (txt.style.visibility === "visible") txt.style.visibility = "hidden"
-		else txt.style.visibility = "visible"
+
+	if (count >= 45 * 6) count = 0
+
+	if (count % 45 === 0) {
+		txt1.style.visibility = "hidden"
+		txt2.style.visibility = "hidden"
+		txt3.style.visibility = "hidden"
+
+		if (count === 45) txt1.style.visibility = "visible"
+		if (count === 45 * 3) txt2.style.visibility = "visible"
+		if (count === 45 * 5) txt3.style.visibility = "visible"
 	}
+
 	requestAnimationFrame(main)
 }
 
