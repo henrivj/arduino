032bf25eb8100a04a8b4a3320c720c3584a79a45
henrivj <henrique_justus@estudante.sesisenai.org.br>
2026-08-20 08:28:02 -0300

    v5 fix

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
-const txt1 = document.getElementById("txt1")
-const txt2 = document.getElementById("txt2")
-const txt3 = document.getElementById("txt3")
+const txt = document.getElementById("txt")
 
 let count = 0
 
 function main() {
 	count++
 
-	if (count >= 45 * 6) count = 0
-
-	if (count % 45 === 0) {
-		txt1.style.visibility = "hidden"
-		txt2.style.visibility = "hidden"
-		txt3.style.visibility = "hidden"
-
-		if (count === 45) txt1.style.visibility = "visible"
-		if (count === 45 * 3) txt2.style.visibility = "visible"
-		if (count === 45 * 5) txt3.style.visibility = "visible"
+	if (count >= 45) {
+		if (txt.style.visibility === "visible") txt.style.visibility = "hidden"
+		else txt.style.visibility = "visible"
+		count = 0
 	}
 
 	requestAnimationFrame(main)
